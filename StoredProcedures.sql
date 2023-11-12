73b474b52d9aa5785bd7f57f332a075c8c02d2ec
Osman100 <36994519+Osman100@users.noreply.github.com>
2023-11-12 05:40:18 -0800

    SQL actualizacion

--- StoredProcedures.sql
+++ StoredProcedures.sql
@@ -1,9 +1,9 @@
-CREATE PROCEDURE decreaseStock @idProducto int
+CREATE PROCEDURE decreaseStock @idProducto int, @cantidad int
 AS 
-UPDATE Producto SET CantidadStock = CantidadStock - 1 WHERE idProducto = @idProducto;
+UPDATE Producto SET CantidadStock = CantidadStock - @cantidad WHERE idProducto = @idProducto;
 
 GO 
 
-CREATE PROCEDURE increaseStock @idProducto int
+CREATE PROCEDURE increaseStock @idProducto int, @cantidad int
 AS 
-UPDATE Producto SET CantidadStock = CantidadStock + 1 WHERE idProducto = @idProducto;
+UPDATE Producto SET CantidadStock = CantidadStock + @cantidad WHERE idProducto = @idProducto;
